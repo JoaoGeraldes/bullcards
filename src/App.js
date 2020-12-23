a0243438af21126d324428a39c8f41ef5bf2b316
none <46204001+JoaoGeraldes@users.noreply.github.com>
2020-12-23 00:03:31 +0000

    Added memoization for performance

--- src/App.js
+++ src/App.js
@@ -4,37 +4,43 @@ import Play from "./components/Play";
 import Score from "./components/Score";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Menu from "./components/Menu";
+import { useDispatch, useSelector } from "react-redux";
+import { useMemo } from "react";
 
 function App() {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  /* ----------------------------- */
+  /* ---- MEMOIZED COMPONENTS ---- */
+  /* ----------------------------- */
+  const ScoreMemoized = useMemo(() => {
+    return <Score />;
+  }, [state.scoreboard]);
+
+  const PlayMemoized = useMemo(() => {
+    return <Play />;
+  }, [state.board]);
+
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/play">Play</Link>
-            </li>
-            <li>
-              <Link to="/score">Score</Link>
-            </li>
-          </ul>
-        </nav>
-
+      <div className="container">
+        {state.isMenuOpen && <Menu />}
+        <h2
+          style={{ cursor: "pointer" }}
+          onClick={() => dispatch({ type: "TOGGLE_MENU" })}
+        >
+          &#x02261; MENU
+        </h2>
         {/* A <Switch> looks through its children <Route>s and
         renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/start">
             <Start />
           </Route>
-          <Route path="/play">
-            <Play />
-          </Route>
-          <Route path="/score">
-            <Score />
-          </Route>
+          <Route path="/play">{PlayMemoized}</Route>
+          <Route path="/score">{ScoreMemoized}</Route>
           <Route path="/">
             <Start />
           </Route>
